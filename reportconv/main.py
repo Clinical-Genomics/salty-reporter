0f0016f580b16ac0a2b7500197bb41855d8b5220
Samuel Lampa <samuel.lampa@scilifelab.se>
2023-08-16 15:28:57 +0200

    Read in JSON data

--- reportconv/main.py
+++ reportconv/main.py
@@ -1,1 +1,19 @@
-# TBC
+import argparse
+import json
+
+argp = argparse.ArgumentParser()
+argp.add_argument(
+    "-i", "--input-file", help="Input JASEN report file in JSON format", required=True
+)
+args = argp.parse_args()
+
+
+def main():
+    with open(args.input_file) as jasen_report_f:
+        jasen_report = json.load(jasen_report_f)
+    jasen_report_str = json.dumps(jasen_report, indent=8)
+    print(jasen_report_str)
+
+
+if __name__ == "__main__":
+    main()
